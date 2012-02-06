6dd6568a6766b3d8e1e38b41975080813957d654
zhygr <zhygrr@gmail.com>
2012-02-07 01:48:31 +0300

    - Allow print debug information about the money value in base currency (currently EUR).   We require this functionality in order to check the correctness of the sorting   algorithm that takes into account real-time values for exchange rates.

--- src/sellstome/search/solr/response/json/converters/MoneySolrFieldConverter.scala
+++ src/sellstome/search/solr/response/json/converters/MoneySolrFieldConverter.scala
@@ -2,14 +2,26 @@ package sellstome.search.solr.response.json.converters
 
 import org.apache.solr.schema.FieldType
 import org.apache.lucene.index.IndexableField
-import sellstome.search.solr.common.NotImplementedException
+import sellstome.search.solr.schema.MoneyType
+import org.json.JSONObject
+import sellstome.search.solr.schema.finance.MoneyValue
 
 /**
- *
+ * Converts a money stored value to a json representation.
  * @author Aliaksandr Zhuhrou
  */
 object MoneySolrFieldConverter extends SolrField2JsonConverter {
-  def toJson(fieldType: FieldType, indexableField: IndexableField) =
-    //todo zhugrov a - provide the implementation of this method
-    indexableField.stringValue()
+  def toJson(fieldType: FieldType, indexableField: IndexableField): JSONObject = fieldType match {
+    case moneyType: MoneyType => {
+      val moneyValue = MoneyValue.parse(indexableField.stringValue())
+      val price = new JSONObject()
+      price.put("amount", moneyValue.getAmount)
+      price.put("currency", moneyValue.getCurrency.getCurrencyCode())
+      if (moneyType.isDebugEnabled()) {
+        price.put("amountInBaseCurrency", MoneyType.ExchangeRatesService.convertCurrency(moneyValue , MoneyType.BaseCurrency))
+      }
+      return price
+    }
+    case _  => throw new IllegalArgumentException("This converter doesn't support a given fieldType: " + fieldType.getClass())
+  }
 }
--- src/sellstome/search/solr/schema/MoneyType.scala
+++ src/sellstome/search/solr/schema/MoneyType.scala
@@ -6,7 +6,6 @@ import org.apache.lucene.document.FieldType
 import org.apache.solr.search.function.ValueSourceRangeFilter
 import org.apache.lucene.search.{SortField, Query}
 import org.apache.solr.common.SolrException
-import java.io.IOException
 import org.apache.solr.schema.{SchemaField, IndexSchema, AbstractSubTypeFieldType}
 import org.apache.solr.response.TextResponseWriter
 import org.apache.solr.search.{QParser, SolrConstantScoreQuery}
@@ -15,23 +14,33 @@ import com.google.common.collect.Lists
 import org.apache.lucene.queries.function.ValueSource
 import sellstome.search.solr.service.finance.CurrencyExchangeRatesService
 import sellstome.search.solr.util.Currencies
-import sellstome.search.solr.common.trysolr
-
+import beans.BooleanBeanProperty
 
 object MoneyType {
   /** logger instance */
   private[schema] val log: Logger = LoggerFactory.getLogger(classOf[MoneyType])
   /** todo zhugrov a - think how to refactor it via dependency injection pattern */
-  private[schema] val exchangeRatesService = new CurrencyExchangeRatesService()
+  val ExchangeRatesService = new CurrencyExchangeRatesService()
   /** base currency */
-  private[schema] val BaseCurrency = Currencies("EUR")
+  val BaseCurrency = Currencies("EUR")
+  /** is debug parameter name for a given SchemaField */
+  private[schema] val DebugEnabled = "debug"
 }
 
 /** Field type for support of monetary values. */
 class MoneyType extends AbstractSubTypeFieldType {
 
+  /** whenever we print the debug information */
+  @BooleanBeanProperty
+  var debugEnabled = false
+
   protected override def init(schema: IndexSchema, args: java.util.Map[String, String]) {
     super.init(schema, args)
+    val debugEnabled = args.get(MoneyType.DebugEnabled)
+    if (debugEnabled != null) {
+      this.debugEnabled = debugEnabled.toBoolean
+      args.remove(MoneyType.DebugEnabled)
+    }
     createSuffixCache(1)
   }
 
@@ -40,7 +49,7 @@ class MoneyType extends AbstractSubTypeFieldType {
   override def createFields(field: SchemaField, externalVal: AnyRef, boost: Float): Array[IndexableField] = {
     val f: Array[IndexableField] = new Array[IndexableField]((if (field.indexed) 2 else 0) + (if (field.stored) 1 else 0))
     if (field.indexed) {
-      val value = MoneyType.exchangeRatesService.convertCurrency(MoneyValue.parse(externalVal.toString), MoneyType.BaseCurrency)
+      val value = MoneyType.ExchangeRatesService.convertCurrency(MoneyValue.parse(externalVal.toString), MoneyType.BaseCurrency)
       f(0) = subField(field, 0).createField(String.valueOf(value.getAmount), boost)
     }
     if (field.stored) {
@@ -77,7 +86,7 @@ class MoneyType extends AbstractSubTypeFieldType {
    * It never makes sense to create a single field, so make it impossible to happen
    */
   override def createField(field: SchemaField, value: AnyRef, boost: Float): IndexableField = {
-    throw new UnsupportedOperationException("LatLonType uses multiple fields.  field=" + field.getName)
+    throw new UnsupportedOperationException("LatLonType uses multiple fields. Field=" + field.getName)
   }
 
   /** How to use this method to deserialize the corresponding indexable field */
