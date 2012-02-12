3f758c47e0aa40fa21f263512c39ba50f6ce92c9
zhygr <zhygrr@gmail.com>
2012-02-12 13:53:29 +0300

    - remove comparing by raw (amount,currency).

--- src/sellstome/search/solr/schema/finance/MoneyFieldComparatorSource.scala
+++ src/sellstome/search/solr/schema/finance/MoneyFieldComparatorSource.scala
@@ -42,22 +42,10 @@ class MoneyFieldComparatorSource(exchangeRatesService: ICurrencyExchangeRateServ
     private var bottom: Long                                      = 0L
     private var docsWithField: Bits                               = null
     private val primaryParser                                     = FieldCache.NUMERIC_UTILS_LONG_PARSER
-    private var secondaryFieldReader: PerSegmentMoneyFieldReader  = null
-
-    /**
-     * As you probably know from MoneyType we use two lucene IndexedField in order to store money value.
-     * The first field stores a long amount value in base currency. It used for fast primary lookup.
-     * And the second value encodes money amount in original currency as long number (@see sellstome.search.solr.schema.finance.MoneyValue#convertToMinorCurrency)
-     * and ISO4217 Currency code.
-     * So we need to fetch the value of second field and manually decode it as money value.
-     */
-    private var reader:PerSegmentMoneyFieldReader = null
 
     def compare(firstSlot: Int, secondSlot: Int): Int = {
       val firstValue  = values(firstSlot)
       val secondValue = values(secondSlot)
-      val firstMoneyValue  = secondaryFieldReader.moneyValue(firstSlot)
-      val secondMoneyValue = secondaryFieldReader.moneyValue(secondSlot)
 
       return Ordering.Long.compare(firstValue, secondValue)
     }
@@ -80,29 +68,11 @@ class MoneyFieldComparatorSource(exchangeRatesService: ICurrencyExchangeRateServ
         v2 = missingValue
       }
       values(slot) = v2
-      secondaryFieldReader.copy(slot, doc)
     }
 
     def setNextReader(context: AtomicReaderContext): MoneyFieldComparator = {
       currentReaderValues = FieldCache.DEFAULT.getLongs(context.reader, primaryField, primaryParser, true)
       docsWithField       = FieldCache.DEFAULT.getDocsWithField(context.reader, primaryField)
-      if (secondaryField.isDefined) {
-        val termsIndex    = FieldCache.DEFAULT.getTermsIndex(context.reader, secondaryField.get)
-        val docToOrd      = termsIndex.getDocToOrd()
-        
-        if (docToOrd.hasArray()) {
-           docToOrd.getArray match {
-             case readerOrds: Array[Byte]  => {secondaryFieldReader = new ByteOrdMoneyFieldReader( numHits, termsIndex, readerOrds)}
-             case readerOrds: Array[Short] => {secondaryFieldReader = new ShortOrdMoneyFieldReader(numHits, termsIndex, readerOrds)}
-             case readerOrds: Array[Int]   => {secondaryFieldReader = new IntOrdMoneyFieldReader(  numHits, termsIndex, readerOrds)}
-           }
-        }
-        
-        if (secondaryFieldReader == null) {
-           secondaryFieldReader = new AnyOrdMoneyReader(numHits, termsIndex, docToOrd)
-        }
-      }
-      
       if (docsWithField.isInstanceOf[Bits.MatchAllBits]) { docsWithField = null }
       return this
     }
@@ -111,95 +81,4 @@ class MoneyFieldComparatorSource(exchangeRatesService: ICurrencyExchangeRateServ
 
   }
 
-  /**
-   * Reads lucene field as money value.
-   * Tightly integrated with comparator API.
-   */
-  private abstract class PerSegmentMoneyFieldReader(numHits: Int, termsIndex: FieldCache.DocTermsIndex) {
-
-    /** Stores references to a given field in a given document value. */
-    protected val values: Array[BytesRef] = new Array[BytesRef](numHits)
-    
-    /** Adds a value for a given slot */
-    def copy(slot: Int, doc: Int): Unit
-
-    /** Returns value for a given slot */
-    def value(slot: Int): BytesRef = values(slot)
-
-    /** Returns value for a given slot as amount&currency pair */
-    def moneyValue(slot: Int): (Long, Currency) = MoneyValue.parse(value(slot))
-
-  }
-  
-  private final class   ByteOrdMoneyFieldReader(numHits: Int, termsIndex: FieldCache.DocTermsIndex, readerOrds: Array[Byte])
-                extends PerSegmentMoneyFieldReader(numHits, termsIndex) {
-
-
-    def copy(slot: Int, doc: Int) {
-      val ord: Int = readerOrds(doc) & 0xFF
-      if (ord == 0) {
-        values(slot) = null
-      }
-      else {
-        if (values(slot) == null) values(slot) = new BytesRef()
-        termsIndex.lookup(ord, values(slot))
-      }
-    }
-
-  }
-  
-  private final class   ShortOrdMoneyFieldReader(numHits: Int, termsIndex: FieldCache.DocTermsIndex, readerOrds: Array[Short])
-                extends PerSegmentMoneyFieldReader(numHits, termsIndex) {
-
-
-    def copy(slot: Int, doc: Int) {
-      val ord: Int = readerOrds(doc) & 0xFFFF
-      if (ord == 0) {
-        values(slot) = null
-      }
-      else {
-        if (values(slot) == null) values(slot) = new BytesRef()
-        termsIndex.lookup(ord, values(slot))
-      }
-    }
-
-  }
-  
-  private final class   IntOrdMoneyFieldReader(numHits: Int, termsIndex: FieldCache.DocTermsIndex, readerOrds: Array[Int])
-                extends PerSegmentMoneyFieldReader(numHits, termsIndex) {
-
-
-    def copy(slot: Int, doc: Int) {
-      val ord: Int = readerOrds(doc)
-      if (ord == 0) {
-        values(slot) = null
-      }
-      else {
-        if (values(slot) == null) {
-          values(slot) = new BytesRef()
-        }
-        termsIndex.lookup(ord, values(slot))
-      }
-    }
-  
-  }
-  
-  private final class   AnyOrdMoneyReader(numHits: Int, termsIndex: FieldCache.DocTermsIndex, readerOrds: PackedInts.Reader)
-                extends PerSegmentMoneyFieldReader(numHits, termsIndex) {
-
-
-    def copy(slot: Int, doc: Int) {
-      val ord: Int = readerOrds.get(doc).asInstanceOf[Int]
-      if (ord == 0) {
-        values(slot) = null
-      }
-      else {
-        if (values(slot) == null) {
-          values(slot) = new BytesRef
-        }
-        termsIndex.lookup(ord, values(slot))
-      }
-    }
-
-  }
 }
