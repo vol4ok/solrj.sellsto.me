7e7c245229a53508d64696a61c5704e6f3bc413e
zhygr <zhygrr@gmail.com>
2012-03-14 22:59:06 +0300

    add support for use simple text codec in solr application.

---
+++ src/sellstome/solr/core/SimpleTextCodecFactory.scala
@@ -0,0 +1,17 @@
+package sellstome.solr.core
+
+import org.apache.solr.core.CodecFactory
+import org.apache.solr.schema.IndexSchema
+import org.apache.lucene.codecs.simpletext.SimpleTextCodec
+import org.apache.lucene.codecs.Codec
+
+/**
+ * Creates a new [[org.apache.lucene.codecs.simpletext.SimpleTextCodec]] instances
+ * @author Aliaksandr Zhuhrou
+ * @since 1.0
+ */
+class SimpleTextCodecFactory extends CodecFactory {
+  /** @return a codec instance */
+  def create(Schema: IndexSchema):Codec = new SimpleTextCodec()
+
+}
