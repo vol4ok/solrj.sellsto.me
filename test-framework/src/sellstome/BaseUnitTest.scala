b64718b9ea9551137a7b9e1680c5bef6a8efa9ca
zhygr <zhygrr@gmail.com>
2012-03-17 16:17:48 +0300

    - update scala-test to 1.7.1 version - add base class for unit tests

---
+++ test-framework/src/sellstome/BaseUnitTest.scala
@@ -0,0 +1,12 @@
+package sellstome
+
+import org.scalatest.FunSuite
+
+/**
+ * Base class that contains a common methods for writing a unit tests
+ * @author Aliaksandr Zhuhrou
+ * @since 1.0
+ */
+class BaseUnitTest extends FunSuite {
+
+}
---
+++ test-src/sellstome/lucene/codecs/values/FindDocValuesSliceInfosUnitTest.scala
@@ -0,0 +1,12 @@
+package sellstome.lucene.codecs.values
+
+import org.scalatest.FunSuite
+
+/**
+ * Tests for [[sellstome.lucene.codecs.values.FindDocValuesSliceInfos]]
+ * @author Aliaksandr Zhuhrou
+ * @since 1.0
+ */
+class FindDocValuesSliceInfosUnitTest extends FunSuite {
+
+}
